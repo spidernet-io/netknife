42c151ae4ec1c67020860f0606115b4265961fcc
weizhou.lan@daocloud.io <weizhou.lan@daocloud.io>
2022-05-02 17:14:51 +0800

    test
    Signed-off-by: weizhou.lan@daocloud.io <weizhou.lan@daocloud.io>

--- images/netknife-base/install-others.sh
+++ images/netknife-base/install-others.sh
@@ -14,6 +14,36 @@ packages=(
   libmnl0
   bash-completion
   iptables
+  # ss
+  iproute2
+  # netstat
+  net-tools
+  arping
+  iftop
+  conntrack
+  ipvsadm
+  lsof
+  iputils-ping
+  iputils-tracepath
+  tcpdump
+  telnet
+  # ssh / ssh-client
+  curl
+  netcat
+  socat
+  # nping
+  nmap
+  ssmping
+  ethtool
+  jq
+  stress-ng
+  # too big
+  #sysstat
+  pciutils
+  iperf3
+  netperf
+  dnsutils
+  dnsperf
 )
 
 
@@ -29,8 +59,45 @@ rm -rf /var/lib/apt/lists/*
 
 #========= verify
 
-iptables -h >/dev/null
-ip6tables -h >/dev/null
-asdf
+iptables-legacy --version
+iptables-nft --version
+ip6tables-legacy --version
+ip6tables-nft --version
+ss -v
+tc -V
+netstat --version
+arping -h
+iftop  -h &>/dev/null
+conntrack --version
+# Can't initialize ipvs: Permission denied (you must be root)
+which ipvsadm >/dev/null
+which telnet >/dev/null
+which nc >/dev/null
+lsof -h >/dev/null
+ping -V
+ping6 -V
+tracepath -V
+tcpdump --version
+curl -V
+socat -V
+nping -V
+nmap -V
+ssmping -v
+ethtool --version
+jq -V
+stress-ng -V
+lspci --version
+iperf3 -v
+netperf -V
+#
+#echo 'ENABLED="true"' > /etc/default/sysstat
+#service sysstat restart
+#sar -V
+#
+dig -v
+dnsperf -h
+
 
 exit 0
+
+
