910f8cac9a16e68fb8c3e75fc71bdfca3f6dcbdd
weizhou.lan@daocloud.io <weizhou.lan@daocloud.io>
2022-05-02 15:30:18 +0800

    test
    Signed-off-by: weizhou.lan@daocloud.io <weizhou.lan@daocloud.io>

--- images/netknife-base/install-others.sh
+++ images/netknife-base/install-others.sh
@@ -18,15 +18,19 @@ packages=(
 
 
 export DEBIAN_FRONTEND=noninteractive
-
 apt-get update
-
-# tzdata is one of the dependencies and a timezone must be set
-# to avoid interactive prompt when it is being installed
 ln -fs /usr/share/zoneinfo/UTC /etc/localtime
-
 apt-get install -y --no-install-recommends "${packages[@]}"
-
 apt-get purge --auto-remove
 apt-get clean
 rm -rf /var/lib/apt/lists/*
+
+
+
+#========= verify
+
+iptables -h >/dev/null
+ip6tables -h >/dev/null
+asdf
+
+exit 0
